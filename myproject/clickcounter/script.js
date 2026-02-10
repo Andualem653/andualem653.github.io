a0a3aff6a56aa4de2f578d07098b2d17f58fdc32
Andualem <130783461+Andualem653@users.noreply.github.com>
2026-02-10 04:40:36 +0300

    fixed "time" typo, changed into times

--- myproject/clickcounter/script.js
+++ myproject/clickcounter/script.js
@@ -4,9 +4,10 @@ let line = document.getElementById("change");
 let count = 0;
 button1.addEventListener("click", function() {
     count = count + 1;
-    line.innerText = `You have clicked the Button ${count} time.`
+    line.innerText = `You have clicked the Button ${count} times.`
 });
 button2.addEventListener("click", function() {
     line.innerText = "You have clicked the Button 0 time."
     count = 0;
+
 });
