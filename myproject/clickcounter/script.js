8e4034939c3332e24cdb186f6e6a00700b916578
Andualem <130783461+Andualem653@users.noreply.github.com>
2026-02-10 04:51:52 +0300

    fixed "1 times" typo in clickcounter

--- myproject/clickcounter/script.js
+++ myproject/clickcounter/script.js
@@ -3,11 +3,15 @@ let button2 = document.getElementById("btn2");
 let line = document.getElementById("change");
 let count = 0;
 button1.addEventListener("click", function() {
-    count = count + 1;
-    line.innerText = `You have clicked the Button ${count} times.`
+    if (count === 0) {
+        count = count + 1;
+        line.innerText = `You have clicked the Button 1 time.`
+    } else {
+        count = count + 1;
+        line.innerText = `You have clicked the Button ${count} times.`
+    }
 });
 button2.addEventListener("click", function() {
     line.innerText = "You have clicked the Button 0 time."
     count = 0;
-
 });
