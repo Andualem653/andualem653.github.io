1c0946b8f0b0f4cf9d738e08ef99f912fbefa15d
Andualem <130783461+Andualem653@users.noreply.github.com>
2026-02-10 01:02:08 +0000

    added js to clickcounter

---
+++ myproject/clickcounter/script.js
@@ -0,0 +1,12 @@
+let button1 = document.getElementById("btn1");
+let button2 = document.getElementById("btn2");
+let line = document.getElementById("change");
+let count = 0;
+button1.addEventListener("click", function() {
+    count = count + 1;
+    line.innerText = `You have clicked the Button ${count} time.`
+});
+button2.addEventListener("click", function() {
+    line.innerText = "You have clicked the Button 0 time."
+    count = 0;
+});
